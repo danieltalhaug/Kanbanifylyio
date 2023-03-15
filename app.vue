f2ff4f10e8534f4de4b527d9fd62346d08ff3dc3
danieltalhaug <daniel.talhaug@outlook.com>
2023-03-15 18:30:30 +0100

    Add app header

--- app.vue
+++ app.vue
@@ -1,10 +1,13 @@
 <template>
   <main class="h-screen bg-slate-50 dark:bg-slate-900 overflow-auto">
+    <KanbanHeader />
     <KanbanBoard />
   </main>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Dongle:wght@300&display=swap');
+
 button {
   @apply p-2 text-slate-600 dark:text-slate-400 hover:text-violet-600 dark:hover:text-violet-400 transition rounded focus:outline outline-2 outline-purple-500 rounded;
 }
---
+++ components/KanbanHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const title: string = 'Kanban-ify-ly...io';
+const subtitles: string[] = [
+    'With a side of sass',
+    'Passive-Aggressive Productivity',
+    'No more excuses, just Kanban-it',
+    'You\'ll get things done, or else...',
+    'Just a friendly reminder to do your job, no pressure though',
+    'Because clearly, you can\'t manage your own tasks.',
+    'We\'ll remind you of what you forgot to do',
+    'You\'re welcome for the organization',
+    'If you use this for your job we\'ll expect a piece of your salary',
+];
+
+function getRandom(length: number) {
+    return  Math.floor(Math.random() * length);
+}
+</script>
+
+<template>
+    <header class="relative flex flex-col gap-1 relative h-48">
+        <Placeholder :styling="['absolute', 'inset-0', 'w-full', 'h-full']" />
+        <div class="absolute inset-0 flex justify-center items-center">
+            <span class="flex flex-col items-center">
+                <h1 class="text-slate-900 dark:text-slate-50 text-8xl font-[Dongle] font-light leading-[0]">
+                    <span>Kanban</span>
+                    <span class="text-6xl">ify</span>
+                    <span class="text-4xl">-ly</span>
+                    <span class="text-xl">...io</span>
+                </h1>
+                <h2 class="text-slate-800 dark:text-slate-200 text-base">
+                    ({{ subtitles[getRandom(subtitles.length)] }})
+                </h2>
+            </span>
+        </div>
+    </header>
+</template>

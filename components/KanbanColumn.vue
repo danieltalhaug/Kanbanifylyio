9a140d246f0d4b6646d337d3526239f58f069f36
danieltalhaug <daniel.talhaug@outlook.com>
2023-03-16 13:18:05 +0100

    Use placeholder when moving a column

--- components/KanbanColumn.vue
+++ components/KanbanColumn.vue
@@ -31,55 +31,69 @@ const borderColor = computed(() => {
 </script>
 
 <template>
-    <article class="flex flex-col min-w-[300px] w-96 border-x border-slate-200 dark:border-slate-700 h-full">
-        <header :class="['flex', 'flex-col', 'pt-1', 'px-1']">
-            <KanbanToolbar
-                :is-addable="true"
-                :is-movable="true"
-                :is-deletable="true"
-                :is-duplicatable="true"
-                :is-paintable="true"
-                context="column"
-                @add="$emit('addTask', columnId)"
-                @duplicate="$emit('duplicateColumn', columnId)"
-                @change-color="$emit('changeColumnColor', columnId)"
-                @delete="$emit('deleteColumn', columnId)"
-            />
-            <span :class="['mx-3', 'border-solid', 'border-b-4', borderColor]">
-                <input
-                    :value="title"
-                    type="text"
-                    class="py-1 text-lg bg-transparent flex-grow focus=border-2 border-t-1 text-slate-900 dark:text-slate-50 focus:outline outline-2 outline-purple-500 rounded truncate"
-                    @keyup.enter="updateTitle(($event.target as HTMLInputElement).value)"
-                    @blur="updateTitle(($event.target as HTMLInputElement).value)"
-                >
-                <p class="text-sm text-slate-600 dark:text-slate-300 mb-4">
-                    <span>Tasks: </span>{{ taskCount }}
-                </p>
-            </span>
-        </header>
-        <div>
+    <article class="column min-w-[300px] w-96 border-x border-slate-200 border-dotted dark:border-slate-700 h-full">
+        <Placeholder :styling="['hidden', 'w-full', 'h-full']" />
 
-        </div>
-        <section class="flex flex-col gap-4 mt-8 grow relative mx-4">
-            <KanbanTaskEmptyState
-                v-if="!taskCount"
-                class="absolute top-[30%]"
-            />
-            <slot />
-        </section>
-
-        <footer class="self-end">
-            <button
-                class="flex gap-2 justify-center"
-                @click="$emit('addTask', columnId)"
-            >
-                <IconPlus
-                    :size="20"
-                    stroke-width="2"
+        <div class="column-content flex flex-col h-full">
+            <header :class="['flex', 'flex-col', 'pt-1', 'px-1']">
+                <KanbanToolbar
+                    :is-addable="true"
+                    :is-movable="true"
+                    :is-deletable="true"
+                    :is-duplicatable="true"
+                    :is-paintable="true"
+                    context="column"
+                    @add="$emit('addTask', columnId)"
+                    @duplicate="$emit('duplicateColumn', columnId)"
+                    @change-color="$emit('changeColumnColor', columnId)"
+                    @delete="$emit('deleteColumn', columnId)"
                 />
-                Add task
-            </button>
-        </footer>
+                <span :class="['mx-3', 'border-solid', 'border-b-4', borderColor]">
+                    <input
+                        :value="title"
+                        type="text"
+                        class="py-1 text-lg bg-transparent flex-grow focus=border-2 border-t-1 text-slate-900 dark:text-slate-50 focus:outline outline-2 outline-purple-500 rounded truncate"
+                        @keyup.enter="updateTitle(($event.target as HTMLInputElement).value)"
+                        @blur="updateTitle(($event.target as HTMLInputElement).value)"
+                    >
+                    <p class="text-sm text-slate-600 dark:text-slate-300 mb-4">
+                        <span>Tasks: </span>{{ taskCount }}
+                    </p>
+                </span>
+            </header>
+            
+            <section class="flex flex-col gap-4 mt-8 grow relative mx-4">
+                <KanbanTaskEmptyState
+                    v-if="!taskCount"
+                    class="absolute top-[30%]"
+                />
+                <slot />
+            </section>
+    
+            <footer class="self-end">
+                <button
+                    class="flex gap-2 justify-center"
+                    @click="$emit('addTask', columnId)"
+                >
+                    <IconPlus
+                        :size="20"
+                        stroke-width="2"
+                    />
+                    Add task
+                </button>
+            </footer>
+        </div>
     </article>
 </template>
+
+<style scoped>
+.sortable-ghost {
+    @apply bg-slate-100 dark:bg-slate-900;
+}
+.sortable-ghost .placeholder {
+    @apply block;
+}
+.sortable-ghost .column-content {
+    @apply hidden;
+}
+</style>
